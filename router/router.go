54f8f8278e0bd80c8b977743e4eee4f7ddd78f21
nullconSharique <167956615+nullconSharique@users.noreply.github.com>
2025-01-29 15:00:44 +0530

    routes

---
+++ router/router.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/shariquehaider/ecom-backend/controllers"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.POST("/api/register", controllers.RegisterController)
+	router.POST("/api/login", controllers.LoginController)
+	return router
+}
